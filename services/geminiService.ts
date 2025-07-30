e67ecaeea8cf93a9cfb5eb1ebf0647cd06bc8478
RiffasServices <chetan@xsquaretec.com>
2025-07-31 00:12:52 +0530

    for redeployment

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -5,6 +5,7 @@ if (!process.env.API_KEY) {
     throw new Error("API_KEY environment variable not set");
 }
 
+
 const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
 
 const responseSchema = {
